aa31faf885636b4c36e33eaadaf20bd20719e870
Kenneth-Harper <kenharpe@iu.edu>
2022-05-03 15:06:57 -0400

    Added Wind Zone Object Prefab to Substances

---
+++ Assets/Substances/Wind/Wind.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wind : Substance
+{
+    [SerializeField] float totalForceMultiplier;
+    [SerializeField] GameObject areaOfEffect;
+    
+    [SerializeField] GameObject backPoint;
+    [SerializeField] GameObject frontPoint;
+    
+    Vector2 forceAdded = new Vector2();
+    
+    [SerializeField] float ForceMagnitude;
+
+    void Start()
+    {
+        this.type = "Wind";
+        
+        CalculateForceDirection();
+    }
+
+    void Update()
+    {
+        CalculateForceDirection();
+    }
+
+
+    void CalculateForceDirection()
+    {
+        // Vector2 Direction = new Vector2(areaOfEffect.GetComponent<Renderer>().bounds.max.x, areaOfEffect.GetComponent<Renderer>().bounds.max.y) - new Vector2(areaOfEffect.GetComponent<Renderer>().bounds.min.x, areaOfEffect.GetComponent<Renderer>().bounds.max.y); 
+        
+        Vector2 Direction = new Vector2();
+
+        Vector3 pseudoDirection =frontPoint.transform.position - backPoint.transform.position;
+        Direction = new Vector2(pseudoDirection.x, pseudoDirection.y);
+        
+        Direction.Normalize();
+
+
+        forceAdded = Direction * totalForceMultiplier;
+
+        ForceMagnitude = forceAdded.magnitude;
+
+        Debug.DrawRay(areaOfEffect.GetComponent<Renderer>().bounds.center, forceAdded);
+    }
+
+    private void OnTriggerStay2D(Collider2D other) 
+    {
+        if (other.gameObject.GetComponent<Substance>() != null)
+        { 
+            Substance triggerSubstance = other.gameObject.GetComponent<Substance>();
+            SubstanceInteract(triggerSubstance);
+        }
+    }
+    public override void SubstanceInteract(Substance triggerSubstance)
+    {
+        if (triggerSubstance.gameObject.GetComponent<Rigidbody2D>() != null)
+        {
+            triggerSubstance.gameObject.GetComponent<Rigidbody2D>().AddForce(forceAdded);
+        }
+    }
+}
